6c443be0a68aa222d7a258481f1b46b5bfc8ef38
Jonathan Clarke <jonathan.clarke@mymona.uwi.edu>
2019-02-17 06:20:21 +0000

    Info3180 Lab 3

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,10 @@
 from flask import Flask
-
+from flask_mail import Mail
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'jonno'
+app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
+app.config['MAIL_PORT'] = '2525' # (or try 2525)
+app.config['MAIL_USERNAME'] = 'd8d8727929223a'
+app.config['MAIL_PASSWORD'] = '21461frun014a25c3'
+mail = Mail(app)
 from app import views
--- app/views.py
+++ app/views.py
@@ -7,12 +7,17 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from app.forms import EmailPasswordForm
+from app import mail
+from flask_mail import Message
+
+
 
 
 ###
 # Routing for your application.
 ###
-
+    
 @app.route('/')
 def home():
     """Render website's home page."""
@@ -24,6 +29,18 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/contact/', methods=["GET", "POST"])
+def contact():
+    form = EmailPasswordForm()
+    
+    if form.validate_on_submit():
+        msg = Message(form.subject.data, sender=(form.name.data,form.email.data),recipients=["to@example.com"])
+        msg.body = 'This is the body of the message'
+        mail.send(msg)
+        
+        return redirect(url_for('home'))
+        
+    return render_template('contact.html', form=form)
 
 ###
 # The functions below should be applicable to all Flask apps.
